e05ee6d2f766af8c27ccc8649269cefb211cb6ee
Iago Intra <iago2vix@gmail.com>
2024-09-30 17:16:15 -0300

    fix: changes the script to english

--- ResidentEvil/data-collect.py
+++ ResidentEvil/data-collect.py
@@ -9,52 +9,52 @@ def get_response(url):
     resp = requests.get(url)
     return resp
     
-def get_info_basicas(soup):
+def get_basic_info(soup):
     ems = (soup.find("div", class_ ="td-page-content")
                .find_all("p")[1]
                .find_all("em"))
     data = {}
     for i in ems:
-        chave, valor, *_ = i.text.split(":")
-        data[chave.strip(" ")] = valor.strip(" ")
+        key, value, *_ = i.text.split(":")
+        data[key.strip(" ")] = value.strip(" ")
     return data
     
-def get_aparicoes_personagens(soup):
+def get_appearances(soup):
     lis = (soup.find("div", class_ ="td-page-content")
                .find("h4")
                .find_next()
                .find_all("li"))
-    aparicoes = [i.text for i in lis]
-    return aparicoes
+    appearances = [i.text for i in lis]
+    return appearances
 
-def get_personagens(url):
+def get_characters(url):
     resp = get_response(url)
     if resp.status_code != 200:
-       print(f"Erro, código: {resp.status_code}, {url}")
+       print(f"Error, code: {resp.status_code}, {url}")
        return {}
     else:
         soup = BeautifulSoup(resp.text, "html.parser")
-        data = get_info_basicas(soup)
-        data["Aparicoes"] = get_aparicoes_personagens(soup)
+        data = get_basic_info(soup)
+        data["Appearances"] = get_characters(soup)
         return data
 
 def get_links():
     url = "https://www.residentevildatabase.com/personagens/"
     resp = requests.get(url, "html.parser")
-    soup_personagens = BeautifulSoup(resp.text)
-    ancoras = (soup_personagens.find("div", class_="td-page-content")
-                               .find_all("a"))
-    links = [i["href"] for i in ancoras]
+    soup_characters = BeautifulSoup(resp.text)
+    anchors = (soup_characters.find("div", class_="td-page-content")
+                              .find_all("a"))
+    links = [i["href"] for i in anchors]
     return links
 
 # %%
 links = get_links()
 data = []
 for link in tqdm(links):
-    d = get_personagens(link)
+    d = get_characters(link)
     d["Link"] = link
     data.append(d)
 
 # %%
 df = pd.DataFrame(data)
-df.to_parquet("dados_re.parquet", index=False)
+df.to_parquet("re_characters_data.parquet", index=False)
